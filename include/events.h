fdf261bdc29bedd0dbcf35c80677b7b9373682ac
Alef Farah <afarah1@users.noreply.github.com>
2016-06-01 13:41:31 -0300

    Improved docs

--- include/events.h
+++ include/events.h
@@ -6,6 +6,19 @@
 #include <stdint.h>
 #include <stddef.h>
 
+/*
+ * Note on copies of structure fields:
+ *
+ * Pointers to ref counted structs are copied as is, no new structure is
+ * created (the referenced struct, if any, has its ref ct increased).
+ *
+ * Strings are copied with strcpy.
+ *
+ * Thus, all copies are neither completely deep nor shallow.
+ */
+
+// FIXME dont abort on failures
+
 /* A link, as read from a pj_dump trace */
 struct Link {
   struct ref ref;
@@ -18,35 +31,40 @@ struct Link {
 
 /*
  * Create a link from a line from a pj_dump trace. Returns NULL if the line is
- * not describing a link
+ * not describing a link. Reports other failures, possibly aborting.
  */
 struct Link *
 link_from_line(char *line);
 
 struct State;
 
-/*
- * Represents a communication to avoid Link queues for such a task. match holds
- * a copy of the matching state in the communication, c_match holds a pointer
- * to the original matching state, so we know when it's compensated ; strings
- * are copied and ref cts inc, as per usual
- */
+/* Represents a communication to avoid Link queues for such a task */
 struct Comm {
+  /*
+   * match: copy of the matching state in the communication
+   * cmatch: pointer to the original matching state
+   */
   struct State *match, *c_match;
   char *container;
   size_t bytes;
 };
 
+/* Creates a new comm struct, aborts on failure. */
 struct Comm *
 comm_new(struct State *match, char const *container, size_t bytes);
 
+/* Deletes a (valid) comm. Aborts on failure. */
 void
 comm_del(struct Comm *comm);
 
+/*
+ * Returns true if event in comm has been compensated, false otherwise. Aborts
+ * on failure.
+ */
 bool
 comm_compensated(struct Comm const *comm);
 
-/* Deep copy of non ref counted ptrs (char *, etc), shallow otherwise */
+/* Copy a comm struct, aborts on failure. */
 struct Comm *
 comm_cpy(struct Comm const *comm);
 
@@ -59,13 +77,18 @@ struct State {
   struct Comm *comm;
 };
 
+/*
+ * Create a state from a line from a pj_dump trace. Returns NULL if the line is
+ * not describing a state. Reports other failures, possibly aborting.
+ */
 struct State *
 state_from_line(char *line);
 
-/* Deep copy of non ref counted ptrs (char *, etc), shallow otherwise */
+/* Copy a state struct, aborts on failure. */
 struct State *
 state_cpy(struct State const *state);
 
+/* Prints a state to stdout pj_dump style. Aborts on failure. */
 void
 state_print(struct State const *state);
 
@@ -73,14 +96,24 @@ state_print(struct State const *state);
 void
 state_print_c_recv(struct State const *state);
 
+/* Returns true if state is a P2P recv, false otherwise. Aborts on failure. */
 bool
 state_is_recv(struct State const *state);
 
+/* Returns true if state is a P2P send, false otherwise. Aborts on failure. */
 bool
 state_is_send(struct State const *state);
 
+/*
+ * Returns true if state is a P2P send and is synchronous, false otherwise.
+ * Aborts on failure.
+ */
 bool
 state_is_ssend(struct State const *state, size_t sync_size);
 
+/*
+ * Returns true if state is a P2P send and is asynchronous, false otherwise.
+ * Aborts on failure.
+ */
 bool
 state_is_asend(struct State const *state, size_t sync_size);
